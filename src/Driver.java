f4fe8e0927d632672bd8de96345a034ddea04b31
0022Xiong <202483920022@nuist.edu.cn>
2024-10-28 21:38:41 +0800

    fill the framework.

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,9 @@
 import java.util.Scanner;
 
 public class Driver {
-    IOModule ioModule = new IOModule();
-    Scanner input = new Scanner(System.in);
+
+    public IOModule ioModule;
+    private Scanner input = new Scanner(System.in);
 
     public static void main(String[] args){
         System.out.println("////////////////////////////////////////");
@@ -12,31 +13,29 @@ public class Driver {
         System.out.println("Loading...");
         Driver driver = new Driver();
 
-
         driver.setup();
     }
 
     public void setup(){
+        System.out.println("Please set the maximum space: ");
+        int maxItem = input.nextInt();
+        ioModule = new IOModule(maxItem);
         System.out.println("Please select the mode:(input(i),search(s),delete(d),output(o)[WIP]");
         char mainMode = input.next().charAt(0);
             if(mainMode == 'i' || mainMode == 'I'){
                 System.out.println("Now in the INPUT mode");
-//                System.out.println( "left");
                 System.out.println("Please choose way to input: type-in(t) or import from .txt(i)[WIP]");
                 char subMode = input.next().charAt(0);
                 if(subMode == 't' || subMode == 'T'){
                     ioModule.itemTypeIn();
-                    System.out.println("Finish!");
-                    System.out.println("Preview: ");
-                    System.out.print(ioModule.getItemList());
-                }
+                    }
+
 //                if(subMode == 'i' || subMode == 'I'){
 //
 //                }
                 else{
                     System.out.println("Invalid word");
                 }//input end
-
 
             }
             else if(mainMode == 's' || mainMode == 'S'){
@@ -47,9 +46,6 @@ public class Driver {
             else if(mainMode == 'd' || mainMode == 'D'){
                 System.out.println("Now in the DELETE mode");
                 ioModule.itemDelete();
-                System.out.println("Finish!");
-                System.out.println("Preview: ");
-                System.out.println(ioModule.getItemList());
 
             }
 //            if(mode == 'o' || mode == 'O'){
--- src/IOModule.java
+++ src/IOModule.java
@@ -1,40 +1,61 @@
 import java.util.Scanner;
 
 public class IOModule {
-//    Storage storage = new Storage();
     Scanner input = new Scanner(System.in);
 
-    Storage storage[];
-    int maxItem = 0;
+    private Storage storage[];
+    private int maxItem;
+
+    public IOModule(int capability){
+        storage = new Storage[capability];
+    }
+
     int currentItem = 0;
-
-    public void itemTypeIn(){
-        System.out.println("Please enter the name of the item: ");
-        String itemName = input.nextLine();
-        System.out.println("Please enter the stock of the item: ");
-        int itemNum = input.nextInt();
-        System.out.println("Please enter the price of the item: ");
-        Double itemPrice = input.nextDouble();
+//    public IOModule() {
+        public void itemTypeIn () {
+            System.out.println("Please enter the name of the item: ");
+            String itemName = input.nextLine();
+            System.out.println("Please enter the stock of the item: ");
+            int itemNum = input.nextInt();
+            System.out.println("Please enter the price of the item: ");
+            double itemPrice = input.nextDouble();
 //        System.out.println(storage.toString());
-        storage[currentItem] = new Storage(itemName, itemNum, itemPrice);
-        currentItem++;
+            storage[currentItem] = new Storage(itemName, itemNum, itemPrice);
+            currentItem++;
 
-    }
+            System.out.println("Finish!");
+            System.out.println("Preview: ");
+            System.out.print(getItemList());
+            System.out.println(maxItem - currentItem + "line(s) remain");
+            System.out.println("Add more?(Y/n)");
+            char sub2Mode = input.next().charAt(0);
+            if (sub2Mode == 'y' || sub2Mode == 'Y')
+                itemTypeIn();
 
-    public void itemDelete(){
-        System.out.println("Please enter the number you want to delete: ");
-        int num = input.nextInt();
-        storage[num + 1] = null;
-    }
-
-    public String getItemList(){
-        String listOfItem = "";
-        for(int i = 0; i < currentItem; i++){
-            listOfItem += i + ": " + storage[i] + "\n";
         }
-        return listOfItem;
-    }
 
+        public void itemDelete () {
+            System.out.println("Please enter the number you want to delete: ");
+            int num = input.nextInt();
+            storage[num - 1] = null;
+            System.out.println("Finish!");
+            System.out.println("Preview: ");
+            System.out.println(getItemList());
+            System.out.println("Delete more?(Y/n)");
+            char subMode = input.next().charAt(0);
+            if (subMode == 'y' || subMode == 'Y')
+                itemDelete();
+
+        }
+
+        public String getItemList () {
+            String listOfItem = "";
+            for (int i = 1; i <= currentItem + 1; i++) {
+                listOfItem += i + ": " + storage[i - 1] + "\n";
+            }
+            return listOfItem;
+        }
+//    }
 
 
 }
